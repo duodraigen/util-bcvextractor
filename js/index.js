325bd0048700c4452d0d965e458739f0becba67f
Remshi <youremshi@proton.me>
2024-12-15 00:04:24 +0000

    Updated build

--- js/index.js
+++ js/index.js
@@ -1,8 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.getFromCurrency = exports.Banks = exports.Currencies = void 0;
-const currency_table_1 = require("./lib/currency-table");
-Object.defineProperty(exports, "getFromCurrency", { enumerable: true, get: function () { return currency_table_1.getFromCurrency; } });
+exports.getCurrencyTable = exports.getFromCurrency = exports.Currencies = exports.Banks = void 0;
 const enums_1 = require("./lib/enums");
 Object.defineProperty(exports, "Currencies", { enumerable: true, get: function () { return enums_1.Currencies; } });
 Object.defineProperty(exports, "Banks", { enumerable: true, get: function () { return enums_1.Banks; } });
+const currency_table_1 = require("./lib/currency-table");
+Object.defineProperty(exports, "getFromCurrency", { enumerable: true, get: function () { return currency_table_1.getFromCurrency; } });
+Object.defineProperty(exports, "getCurrencyTable", { enumerable: true, get: function () { return currency_table_1.getCurrencyTable; } });
--- js/lib/currency-table.js
+++ js/lib/currency-table.js
@@ -14,10 +14,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getFromCurrency = getFromCurrency;
 exports.getCurrencyTable = getCurrencyTable;
-const config_1 = require("./config");
-const enums_1 = require("./enums");
 const axios_1 = __importDefault(require("axios"));
 const node_html_parser_1 = require("node-html-parser");
+const enums_1 = require("./enums");
+const config_1 = require("./config");
 function getFromCurrency(currency) {
     return __awaiter(this, void 0, void 0, function* () {
         try {
