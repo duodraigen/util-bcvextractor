16c423c997ccc7993af46983a7eae13624c01f6f
duodraigen <youremshi@proton.me>
2025-01-08 23:27:42 -0400

    updated index of types/methods

--- index.ts
+++ index.ts
@@ -1,12 +1,17 @@
 import { Currencies, Banks } from "@lib/enums";
-import { USDBankInfo, CurrencyTable } from "@lib/types";
+import { getBanksTable, getBankFromTable } from "@lib/banks-table";
 import { getFromCurrency, getCurrencyTable } from "@lib/currency-table";
+import { USDInfo, CurrencyTable, BankInfo, BankTable } from "@lib/types";
 
 export {
 	Banks,
+	USDInfo,
+	BankInfo,
+	BankTable,
 	Currencies,
-	USDBankInfo,
 	CurrencyTable,
+	getBanksTable,
 	getFromCurrency,
+	getBankFromTable,
 	getCurrencyTable,
 };
