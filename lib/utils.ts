2f6a67f4f4ce056a4ada5ced146feaeec00647ce
duodraigen <youremshi@proton.me>
2025-01-08 23:25:31 -0400

    added decodeRow implementation for raw row data

--- lib/utils.ts
+++ lib/utils.ts
@@ -6,7 +6,7 @@
 export function toFloat(str?: string): number {
 	if (!str) return Number.NaN;
 	/**
-	 *  Matches a float number with or without comma.
+	 * Matches a float number with or without comma.
 	 * @example
 	 * FLOAT_REGEX.test('123') // => true
 	 * FLOAT_REGEX.test('123.456') // => true
@@ -16,4 +16,14 @@ export function toFloat(str?: string): number {
 	const num = str.match(FLOAT_REGEX)?.[0];
 	if (!num) return Number.NaN;
 	return Number.parseFloat(num.replace(",", "."));
+}
+
+/**
+ * Takes a raw table data and separates each data into separated cells
+ * @param { string } rawStr - raw table row string
+ * @returns { string[] } row cells
+ */
+export function decodeRow(rawStr: string): string[] {
+	const FILL_REGEX = /\n|\s{2,}/gm;
+	return rawStr.split(FILL_REGEX).filter((item) => item !== "");
 }
